e9213368fc041442620f713980fc5b9b2a36ce0f
MutheuLinet <muneenilinet@gmail.com>
2022-04-08 18:34:27 +0300

    Add components and directives

---
+++ src/app/goal-detail/goal-detail.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Goal } from '../goal';
+
+@Component({
+  selector: 'app-goal-detail',
+  templateUrl: './goal-detail.component.html',
+  styleUrls: ['./goal-detail.component.css']
+})
+export class GoalDetailComponent implements OnInit {
+
+  @Input() 'goal': Goal;
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  goalDelete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+// import { Component, Input, OnInit } from '@angular/core';
+// import { Goal } from '../goal';
+
+// @Component({
+//   selector: 'app-goal-detail',
+//   templateUrl: './goal-detail.component.html',
+//   styleUrls: ['./goal-detail.component.css']
+// })
+// export class GoalComponent implements OnInit {
+  
+  
+//     toggleDetails(index){
+//       this.goals[index].showDescription = !this.goals[index].showDescription;
+//     }
+  
+//     constructor() { }
+  
+//     ngOnInit() {
+//     }
+  
+//   }
+  
+// export class GoalDetailComponent implements OnInit {
+
+//   @Input() 'goal': Goal;
+//   constructor() { }
+
+//   ngOnInit(): void {
+//   }
+
+// }
