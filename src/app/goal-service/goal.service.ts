28462311c4854a3d9205b23cf81aecf44eb9b641
MutheuLinet <muneenilinet@gmail.com>
2022-04-18 17:28:46 +0300

    Add various changes

---
+++ src/app/goal-service/goal.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Goals } from '../goalList';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GoalService {
+
+  getGoals(){
+    return Goals
+  }
+  
+  getGoal(id:any){
+    for (let goal of Goals){
+      if (goal.id == id){
+        return goal;
+      }
+    }return(id)
+  }
+  
+  constructor() { }
+}
--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,44 +1,81 @@
+import { Goals } from './../goalList';
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
 import { Goal } from '../goal';
+import { GoalService } from '../goal-service/goal.service';
+import { Quote } from '../quote-class/quote';
+import { QuoteRequestService } from '../quote-http/quote-request.service';
+
 
 @Component({
   selector: 'app-goal',
   templateUrl: './goal.component.html',
-  styleUrls: ['./goal.component.css']
+  styleUrls: ['./goal.component.css'],
+  providers: [GoalService]
 })
-export class GoalComponent implements OnInit {
-
-  goals: Goal[] = [
-    new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2019,9,14)),
-    new Goal(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
-    new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2019,1,12)),
-    new Goal(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,11,18)),
-    new Goal(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
-    new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2019,3,14)),
-  ];
+export class GoalComponent implements OnInit {  
   
-  addNewGoal(goal: any){
+  goals:Goal[];
+  quote!:Quote;
+  
+  goToUrl(id:number){
+    this.router.navigate(['/goals',id])
+  }
+
+  deleteGoal(index:number){
+    let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}`)
+
+    if (toDelete){
+      this.goals.splice(index,1)
+    }
+  }
+  addNewGoal(goal:any){
     let goalLength = this.goals.length;
     goal.id = goalLength+1;
     goal.completeDate = new Date(goal.completeDate)
     this.goals.push(goal)
-  }  
-  toggleDetails(index: number){
-    this.goals[index].showDescription = !this.goals[index].showDescription;
-  }  
-
-  deleteGoal(isComplete:boolean, index:number){
-    if(isComplete){
-      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
-
-      if (toDelete){
-        this.goals.splice(index, 1)
-      }
-    }
   }
-  constructor(){}
+  
+  // toggleDetails(index:number){
+  //   this.goals[index].showDescription = !this.goals[index].showDescription;
+  // }  completeGoal(isComplete:boolean, index:number){
+  //   if (isComplete) {
+  //     this.goals.splice(index,1);
+  //   }
+  // }
+  // deleteGoal(isComplete:boolean, index:number){
+  //   if (isComplete) {
+  //     let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)      
+      
+  //     if (toDelete){
+  //       this.goals.splice(index,1)
+  //     }
+  //   }
+  // }
+  constructor(goalService:GoalService, private http:HttpClient, private quoteService:QuoteRequestService, private router:Router) {
+    this.goals = goalService.getGoals()
+  }  
+  
+  
+  ngOnInit() {
 
-  ngOnInit(){
+    this.quoteService.quoteRequest()
+    this.quote = this.quoteService.quote
+
+    interface ApiResponse{
+      author:string;
+      quote:string;
+    }
+
+    this.http.get<ApiResponse>("http://quotes.stormconsultancy.co.uk/random.json").subscribe(data=>{
+      // Succesful API request
+      this.quote = new Quote(data.author, data.quote)
+    },err=>{
+      this.quote = new Quote("Winston Churchill","Never never give up!")
+      console.log("An error occurred")
+    })
   }
 
 }
